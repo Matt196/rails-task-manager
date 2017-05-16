67197e2d9d54fef38b4f210befc17b8e6a10b49b
matthieu reboul <matthieu.reboul@gmail.com>
2017-05-16 11:42:06 +0200

    show list of tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def udpate
+  end
+
+  def destroy
+  end
+end
